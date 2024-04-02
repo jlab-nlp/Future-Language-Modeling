b8a198eac775d0f15fb76c4d20b8d2ac25e54c7f
arianakc <llccmm118@outlook.com>
2024-04-02 00:48:03 -0700

    update processed training data

--- train_future_language_model.sh
+++ train_future_language_model.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=0
 export TRANSFORMERS_CACHE=$(pwd)/transformer_cache
 python3 future_language_model_trainer --model_name_or_path gpt2 \
                    --model_type gpt2 \
-                   --train_file papers_with_year_start_2003_cut_2000_end_2019.csv \
-                   --validation_file papers_with_year_start_2020_cut_2000_end_2020.csv \
+                   --train_file paper_csv/papers_with_year_start_2003_cut_2000_end_2019.csv \
+                   --validation_file paper_csv/papers_with_year_start_2020_cut_2000_end_2020.csv \
                    --per_device_train_batch_size 2 \
                    --per_device_eval_batch_size 2 \
                    --do_train \
